d53812928e4b12cca329cca04cdd41884b4a8676
Quentin Bolsee <quentinbolsee@hotmail.com>
2022-06-13 02:57:14 +0200

    image thershold

--- nodes/nodeList.js
+++ nodes/nodeList.js
@@ -2,10 +2,12 @@ import adder from "./adder.js";
 import number from "./number.js";
 import async_adder from "./async_adder.js";
 import worker from "./worker-example.js";
+import thresholdRGBA from "./thresholdRGBA.js";
 
 export default {
   adder,
   number,
   async_adder,
-  worker
+  worker,
+  thresholdRGBA
 }
--- nodes/thresholdRGBA.js
+++ nodes/thresholdRGBA.js
@@ -1,36 +1,77 @@
-//threshold -> image: RGBA, threshold: (0-1) => image: RGBA
-export const thresholdRGBA = (imageRGBA, threshold) => {
-  const w = imageRGBA.width;
-  const h = imageRGBA.height;
-  const buf = imageRGBA.data;
-  const t = threshold;
+import { render, html, svg } from '../uhtml.js';
 
-  let r, g, b, a, i;
-  for (var row = 0; row < h; ++row) {
-    for (var col = 0; col < w; ++col) {
-      r = buf[(h - 1 - row) * w * 4 + col * 4 + 0];
-      g = buf[(h - 1 - row) * w * 4 + col * 4 + 1];
-      b = buf[(h - 1 - row) * w * 4 + col * 4 + 2];
-      a = buf[(h - 1 - row) * w * 4 + col * 4 + 3];
-      i = (r + g + b) / (3 * 255);
+function workerInternal() {
 
-      let val;
-      if (a === 0) {
-        val = 255;
-      } else if (i > t) {
-        val = 255;
-      } else {
-        val = 0;
+  self.onmessage = function(e) {
+    const inputs = e.data;
+
+    const w = inputs.imageRGBA.width;
+    const h = inputs.imageRGBA.height;
+    const buf = inputs.imageRGBA.data;
+    const t = inputs.threshold;
+
+    let r, g, b, a, i;
+    for (var row = 0; row < h; ++row) {
+      for (var col = 0; col < w; ++col) {
+        r = buf[(h - 1 - row) * w * 4 + col * 4 + 0];
+        g = buf[(h - 1 - row) * w * 4 + col * 4 + 1];
+        b = buf[(h - 1 - row) * w * 4 + col * 4 + 2];
+        a = buf[(h - 1 - row) * w * 4 + col * 4 + 3];
+        i = (r + g + b) / (3 * 255);
+
+        let val;
+        if (a === 0) {
+          val = 255;
+        } else if (i > t) {
+          val = 255;
+        } else {
+          val = 0;
+        }
+
+        buf[(h - 1 - row) * w * 4 + col * 4 + 0] = val;
+        buf[(h - 1 - row) * w * 4 + col * 4 + 1] = val;
+        buf[(h - 1 - row) * w * 4 + col * 4 + 2] = val;
+        buf[(h - 1 - row) * w * 4 + col * 4 + 3] = 255;
       }
-
-      buf[(h - 1 - row) * w * 4 + col * 4 + 0] = val;
-      buf[(h - 1 - row) * w * 4 + col * 4 + 1] = val;
-      buf[(h - 1 - row) * w * 4 + col * 4 + 2] = val;
-      buf[(h - 1 - row) * w * 4 + col * 4 + 3] = 255;
     }
+
+    self.postMessage([buf, w, h]);
+
+    self.close();
+  };
+
+}
+
+export default {
+  name: "thresholdRGBA",
+  inputs: [
+    { name: "imageRGBA", type: "img" },
+    { name: "threshold", type: "number", input: "box" }
+  ],
+  outputs: [
+    { name: "imageRGBA", type: "img" }
+  ],
+  func: async (img, thresh) => {
+
+    const blob = new Blob(["(" + workerInternal.toString() + "())"]);
+    const url = window.URL.createObjectURL(blob);
+    const worker = new Worker(url);
+
+    const result = new Promise(resolve => {
+      worker.onmessage = e => {
+        const message = e.data;
+
+        const img = new ImageData(message[0], message[1], message[2]);
+
+        resolve({"imageRGBA": img});
+
+        // redundant to call this because I call self.close() in internal function
+        // worker.terminate();
+      };
+    })
+
+    worker.postMessage({"imageRGBA": img, "threshold": thresh});
+
+    return await Promise.all([result]);
   }
-
-  const imgdata = new ImageData(buf, w, h);
-
-  return imgdata;
-};
+}
