12569ab1aaad2f405d2f833201853ddc87be39cc
Quentin Bolsee <quentinbolsee@hotmail.com>
2022-06-13 03:13:04 +0200

    img viewer

---
+++ nodes/img_viewer.js
@@ -0,0 +1,26 @@
+import { render, html, svg } from '../uhtml.js';
+
+export default {
+  name: "img viewer",
+  inputs: [
+    { name: "imageRGBA", type: "img" },
+  ],
+  outputs: [
+    { name: "imageRGBA", type: "img" }
+  ],
+  view(node, container) {
+    const img = node.inputs[0];
+    const canvas = document.createElement('canvas');
+    canvas.width = img.width;
+    canvas.height = img.height;
+    var ctx = canvas.getContext("2d");
+    ctx.putImageData(img, 0, 0);
+
+    container.innerHTML = "";
+    container.append(canvas)
+    return "";
+  },
+  func: (x) => {
+    return x;
+  }
+}
--- nodes/nodeList.js
+++ nodes/nodeList.js
@@ -2,12 +2,14 @@ import adder from "./adder.js";
 import number from "./number.js";
 import async_adder from "./async_adder.js";
 import worker from "./worker-example.js";
-import thresholdRGBA from "./thresholdRGBA.js";
+import img_threshold from "./img_threshold.js";
+import img_viewer from "./img_viewer.js";
 
 export default {
   adder,
   number,
   async_adder,
   worker,
-  thresholdRGBA
+  img_threshold,
+  img_viewer
 }
