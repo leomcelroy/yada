d12257f0976aea97e0ecf78ce4f8656d49ea6d2f
leomcelroy <leomcelroy@gmail.com>
2022-06-12 21:20:25 -0400

    Update img_viewer.js

--- nodes/img_viewer.js
+++ nodes/img_viewer.js
@@ -9,12 +9,7 @@ export default {
     { name: "imageRGBA", type: "img" }
   ],
   view(node, container) {
-    if (container == null) {
-        return "";
-    }
 
-    console.log(container);
-      
     const img = node.inputs[0];
     const canvas = document.createElement('canvas');
     canvas.width = img.width;
