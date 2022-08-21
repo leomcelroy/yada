2aa0bdcd7e0ba7daf2eb301737361e7b20b24a4a
Alan Grover <awgrover@gmail.com>
2022-08-21 12:23:48 -0400

    img_offset.js

--- nodeList.js
+++ nodeList.js
@@ -2,15 +2,17 @@
 import adder from "./nodes/adder.js";
 import async_adder from "./nodes/async_adder.js";
 import img_distance from "./nodes/img_distance.js";
+import img_offset from "./nodes/img_offset.js";
+import img_read_png from "./nodes/img_read_png.js";
 import img_threshold from "./nodes/img_threshold.js";
 import img_viewer from "./nodes/img_viewer.js";
 import number from "./nodes/number.js";
 import worker_example from "./nodes/worker-example.js";
-
 export default {
   adder,
   async_adder,
   img_distance,
+  img_offset,
   img_threshold,
   img_viewer,
   number,
---
+++ nodes/img_offset.js
@@ -0,0 +1,92 @@
+// FIXME: what does this "offset" do?
+
+function workerInternal() {
+
+  function transform(img, offset ) {
+    console.log('transform() offset',offset,img);
+    var buf = img.data;
+    var w = img.width;
+    var h = img.height;
+
+    var output = new Uint8ClampedArray(4 * h * w);
+    for (var row = 0; row < h; ++row) {
+      for (var col = 0; col < w; ++col) {
+        if (buf[(h - 1 - row) * w + col] <= offset) {
+          output[(h - 1 - row) * w * 4 + col * 4 + 0] = 255;
+          output[(h - 1 - row) * w * 4 + col * 4 + 1] = 255;
+          output[(h - 1 - row) * w * 4 + col * 4 + 2] = 255;
+          output[(h - 1 - row) * w * 4 + col * 4 + 3] = 255;
+        } else {
+          output[(h - 1 - row) * w * 4 + col * 4 + 0] = 0;
+          output[(h - 1 - row) * w * 4 + col * 4 + 1] = 0;
+          output[(h - 1 - row) * w * 4 + col * 4 + 2] = 0;
+          output[(h - 1 - row) * w * 4 + col * 4 + 3] = 255;
+        }
+      }
+    }
+
+    const imgData = new ImageData(output, w, h);
+
+    return imgData;
+  };
+
+  console.log("worker setup");
+
+  self.onmessage = function(e) {
+    const inputs = e.data;
+    console.log('workerInternal.onmessage',inputs);
+
+    buf = transform(inputs.img, inputs.offset );
+
+    self.postMessage([buf]);
+
+    self.close();
+  };
+
+};
+
+
+export default {
+  name: "img offset",
+  inputs: [
+    { name: "imageRGBA", type: "img_uint8" },
+    { name: "offset", type: "number", input: "box" }
+  ],
+  outputs: [
+    { name: "imageRGBA", type: "img_uint8" }
+  ],
+  onUpdate(node, container) {
+    const img = node.outputs[0];
+
+    const canvas = document.createElement('canvas');
+    canvas.width = img.width;
+    canvas.height = img.height;
+    const imageData = new ImageData(img.data, img.width);
+    const ctx = canvas.getContext("2d");
+    ctx.putImageData(imageData, 0, 0);
+
+    container.innerHTML = "";
+    container.appendChild(canvas);
+  },
+  func: async (img, offset) => {
+
+    const blob = new Blob(["(" + workerInternal.toString() + "())"]);
+    const url = window.URL.createObjectURL(blob);
+    const worker = new Worker(url);
+    console.log("after new Worker");
+
+    const result = new Promise(resolve => {
+      worker.onmessage = e => {
+        console.log("receive worker.onmessage",e.data[0]);
+
+        resolve(e.data[0]);
+      };
+    })
+
+    console.log("work,postMessage", {img, "offset": offset});
+    worker.postMessage({img, offset});
+    console.log("after work,postMessage", {img, offset});
+
+    return await Promise.all([result]);
+  }
+}
