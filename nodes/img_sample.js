9e4a5e4f8528d187d5ef19d2345868fd151c43a6
Alan Grover <awgrover@gmail.com>
2022-08-21 14:05:43 -0400

    change img_sample to non-worker

--- nodes/img_sample.js
+++ nodes/img_sample.js
@@ -4,59 +4,36 @@
 var default_height=100;
 var default_width=100;
 
-function workerInternal() {
 
-  function transform(h,w) {
+function transform(h,w) {
 
-    var buffer = new Uint8ClampedArray(4*h*w);
-    let img = new ImageData(buffer, w, h)
+  const canvas = document.createElement('canvas');
+  canvas.width = w;
+  canvas.height = h;
+  //const canvas = new OffscreenCanvas(w, h);
+  const ctx = canvas.getContext('2d');
+  
+ 
+  ctx.beginPath();
+  ctx.rect(0, 0, w, h);
+  ctx.fillStyle = "black";
+  ctx.fill();
 
-    function setpix(buffer, r,c,color) {
-        i = (r*w + c)*4;
-        for(ci=0;ci<4;ci++) { // r,g,b,a
-          buffer[i+ci] = color[ci]
-        }
-    };
+  var gradient_size = 40;
+  var my_gradient = ctx.createLinearGradient(0, 0, gradient_size, 0);
+  my_gradient.addColorStop(0, "black");
+  my_gradient.addColorStop(0.5 ,"red");
+  my_gradient.addColorStop(1, "green");
+  ctx.fillStyle = my_gradient;
+  ctx.fillRect(0, 0, gradient_size, gradient_size); 
 
-    const canvas = new OffscreenCanvas(w, h);
-    const ctx = canvas.getContext('2d');
-    
-   
-    ctx.beginPath();
-    ctx.rect(0, 0, w, h);
-    ctx.fillStyle = "black";
-    ctx.fill();
+  const margin = 10;
+  ctx.beginPath();
+  ctx.strokeStyle = "#AAAAAA";
+  ctx.arc(h/2, w/2, w/2-margin, 0, 2 * Math.PI);
+  ctx.stroke();
 
-    gradient_size = 40;
-    var my_gradient = ctx.createLinearGradient(0, 0, gradient_size, 0);
-    my_gradient.addColorStop(0, "black");
-    my_gradient.addColorStop(0.5 ,"red");
-    my_gradient.addColorStop(1, "green");
-    ctx.fillStyle = my_gradient;
-    ctx.fillRect(0, 0, gradient_size, gradient_size); 
-
-    const margin = 10;
-    ctx.beginPath();
-    ctx.strokeStyle = "#AAAAAA";
-    ctx.arc(h/2, w/2, w/2-margin, 0, 2 * Math.PI);
-    ctx.stroke();
-
-    return ctx.getImageData(0,0,w,h);
-  };
-
-  console.log("worker setup");
-
-  self.onmessage = function(e) {
-    const inputs = e.data;
-    console.log('workerInternal.onmessage',inputs);
-
-    buf = transform(inputs.width, inputs.height );
-
-    self.postMessage([buf]);
-
-    self.close();
-  };
-
+  return ctx.getImageData(0,0,w,h);
 };
 
 export default {
@@ -83,19 +60,6 @@ export default {
   },
   func: async (height,width) => {
 
-    const blob = new Blob(["(" + workerInternal.toString() + "())"]);
-    const url = window.URL.createObjectURL(blob);
-    const worker = new Worker(url);
-    console.log("after new Worker");
-
-    const result = new Promise(resolve => {
-      worker.onmessage = e => {
-        console.log("receive worker.onmessage",e.data[0]);
-
-        resolve(e.data[0]);
-      };
-    })
-
     // default sizes
     if (height==0) {
       height = default_height;
@@ -104,10 +68,9 @@ export default {
       width = default_height;
       }
 
-    console.log("work,postMessage", {height,width});
-    worker.postMessage({height,width});
-    console.log("after work,postMessage");
-
-    return await Promise.all([result]);
+    // can't do worker, because OffscreenCanvas is poorly supported
+    // and I want to draw shapes
+    var rez= transform(height,width);
+    return [rez];
   }
 }
